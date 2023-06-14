b7036ea0b456108674d631e4c3488420eea0d613
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-15 03:28:10 +0900

    update

--- FileName.cpp
+++ FileName.cpp
@@ -3,12 +3,13 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
-	{
-		return -1;			// エラーが起きたら直ちに終了
-	}
+	if (DxLib_Init() == -1) { return -1; }		
+	// ＤＸライブラリ初期化処理
+	// エラーが起きたら直ちに終了
 
 	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+	DrawPixel(320, 200, GetColor(255, 255, 255));	// 点を打つ
+	DrawPixel(320, 280, GetColor(255, 255, 255));	// 点を打つ
 
 	WaitKey();				// キー入力待ち
 
