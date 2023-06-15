8b0afb4228e53e05f0049ff23cdb48ce2d135b0b
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-15 23:42:05 +0900

    update

--- FileName.cpp
+++ FileName.cpp
@@ -17,7 +17,14 @@ int keyState[256] = { 0 };
 int count = 0;
 
 int shot_img;
+int player_img;
 int ResonanceAtTwilight_audio;
+
+//プレイヤーの座標
+int px = 320;
+int py = 320;
+const int moveSpeed = 4;
+float slow = 1;
 
 //キー入力状態を更新する関数
 void KeyUpdate(void)
@@ -34,7 +41,28 @@ void KeyUpdate(void)
 	}
 }
 
-int x = 100;
+void PlayerMove(void)
+{
+	if (keyState[KEY_INPUT_LSHIFT] > 0) slow = 0.5;
+	else slow = 1;
+
+	if (keyState[KEY_INPUT_RIGHT] > 0 && px < Window_Width) //継続入力
+	{
+		px += moveSpeed * slow;
+	}
+	if (keyState[KEY_INPUT_LEFT] > 0 && px > 0) //継続入力
+	{
+		px -= moveSpeed * slow;
+	}
+	if (keyState[KEY_INPUT_DOWN] > 0 && py < Window_Height) //継続入力
+	{
+		py += moveSpeed * slow;
+	}
+	if (keyState[KEY_INPUT_UP] > 0 && py > 0) //継続入力
+	{
+		py -= moveSpeed * slow;
+	}
+}
 
 void Update(void) //毎フレーム処理
 {
@@ -42,7 +70,7 @@ void Update(void) //毎フレーム処理
 	DrawPixel(Window_Width / 2, Window_Height / 2, GetColor(255, 255, 255));	// 点を打つ
 	DrawCircle(100, 100, 30, GetColor(255, 0, 0), 0);
 	DrawCircle(200, 100, 30, GetColor(0, 255, 0), 1);
-	DrawBox(50, 200, 100, 300, GetColor(0, 0, 255), 1);
+	DrawBox(Window_Width-250, 0, Window_Width, Window_Height, GetColor(0, 0, 255), 1);
 	DrawLine(200, 200, 300, 330, GetColor(255, 255, 255), 1);
 	
 	count++;
@@ -55,23 +83,25 @@ void Update(void) //毎フレーム処理
 	DrawFormatString(100, 100, GetColor(255, 255, 255), "Z KEY %d", keyState[KEY_INPUT_Z]);
 	DrawFormatString(100, 120, GetColor(255, 255, 255), "X KEY %d", keyState[KEY_INPUT_X]);
 
-	if (keyState[KEY_INPUT_Z] > 0) //継続入力
-	{
-		x += 1;
-	}
 	if (keyState[KEY_INPUT_X] == TRUE) //単発入力
 	{
 		StopSoundMem(ResonanceAtTwilight_audio);
+		ChangeVolumeSoundMem(100, ResonanceAtTwilight_audio);
 		PlaySoundMem(ResonanceAtTwilight_audio, DX_PLAYTYPE_BACK);
-		x += 30;
 	}
-	DrawCircle(x, 200, 32, GetColor(255, 255, 0));
+
+	PlayerMove();//プレイヤーの移動
+	DrawRotaGraph(px, py, 1.0, 0, player_img, TRUE); //画像の描画
+	DrawCircle(px, py, 8, GetColor(100, 180, 255)); // プレイヤー
+	
 }
 
 void ImgInit(void)
 {
 	shot_img = LoadGraph("shot.png");
+	player_img = LoadGraph("player.png");
 }
+
 void AudioInit(void)
 {
 	ResonanceAtTwilight_audio = LoadSoundMem("ResonanceAtTwilight.mp3");
@@ -84,7 +114,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 	SetGraphMode(Window_Width, Window_Height, 32);//画面サイズ指定
 	SetOutApplicationLogValidFlag(FALSE);//Log.txtを生成しないように設定
 	SetMainWindowText("鏡像の歌姫 - Reflection of Diva -");
-
 
 	if (DxLib_Init() == -1) { return -1; }		// ＤＸライブラリ初期化処理  エラーが起きたら直ちに終了
 
