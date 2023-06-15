0ac2d5e0036b5216746d40c814ddf52b3026a963
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-15 20:30:14 +0900

    update

--- FileName.cpp
+++ FileName.cpp
@@ -1,19 +1,37 @@
-#include "DxLib.h"
+#include <stdio.h>
+#include <DxLib.h>
+
+//変数宣言エリア
+const int Window_Width = 800;
+const int Window_Height = 600;
+
+void Update(void) //毎フレーム処理
+{
+	DrawPixel(Window_Width / 2, Window_Height / 2 - 20, GetColor(255, 255, 255));	// 点を打つ
+	DrawPixel(Window_Width / 2, Window_Height / 2, GetColor(255, 255, 255));	// 点を打つ
+	DrawPixel(Window_Width / 2, Window_Height / 2 + 20, GetColor(255, 255, 255));	// 点を打つ
+}
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	if (DxLib_Init() == -1) { return -1; }		
-	// ＤＸライブラリ初期化処理
-	// エラーが起きたら直ちに終了
+	ChangeWindowMode(TRUE);//非全画面にセット
+	SetGraphMode(Window_Width, Window_Height, 32);//画面サイズ指定
+	SetOutApplicationLogValidFlag(FALSE);//Log.txtを生成しないように設定
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
-	DrawPixel(320, 200, GetColor(255, 255, 255));	// 点を打つ
-	DrawPixel(320, 280, GetColor(255, 255, 255));	// 点を打つ
+	if (DxLib_Init() == -1) { return -1; }		// ＤＸライブラリ初期化処理  エラーが起きたら直ちに終了
 
-	WaitKey();				// キー入力待ち
+	//画像・音の読み込み
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	while (ProcessMessage() == 0)
+	{
+		ClearDrawScreen();//裏画面消す
+		SetDrawScreen(DX_SCREEN_BACK);//描画先を裏画面に
+		Update();//毎フレーム処理
+		ScreenFlip();//裏画面を表画面にコピー
+	}
+
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
 
 	return 0;				// ソフトの終了 
 }
