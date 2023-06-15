3cf73917a81f968efdb9f2149c041568facd9a64
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-15 20:35:13 +0900

    update

--- FileName.cpp
+++ FileName.cpp
@@ -5,8 +5,12 @@
 const int Window_Width = 800;
 const int Window_Height = 600;
 
+int orivia_img;
+
 void Update(void) //毎フレーム処理
 {
+	DrawGraph(0, 100, orivia_img, 0);
+
 	DrawPixel(Window_Width / 2, Window_Height / 2 - 20, GetColor(255, 255, 255));	// 点を打つ
 	DrawPixel(Window_Width / 2, Window_Height / 2, GetColor(255, 255, 255));	// 点を打つ
 	DrawPixel(Window_Width / 2, Window_Height / 2 + 20, GetColor(255, 255, 255));	// 点を打つ
@@ -17,11 +21,12 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 {
 	ChangeWindowMode(TRUE);//非全画面にセット
 	SetGraphMode(Window_Width, Window_Height, 32);//画面サイズ指定
-	SetOutApplicationLogValidFlag(FALSE);//Log.txtを生成しないように設定
+	SetOutApplicationLogValidFlag(TRUE);//Log.txtを生成しないように設定
 
 	if (DxLib_Init() == -1) { return -1; }		// ＤＸライブラリ初期化処理  エラーが起きたら直ちに終了
 
 	//画像・音の読み込み
+	orivia_img = LoadGraph("orivia.png");
 
 	while (ProcessMessage() == 0)
 	{
