dd2de0594c1a27cc451915af4410fa4e9308dc43
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-15 23:55:05 +0900

    update

--- FileName.cpp
+++ FileName.cpp
@@ -3,8 +3,8 @@
 #define PI 3.141592654
 
 //変数宣言エリア
-const int Window_Width = 800;
-const int Window_Height = 600;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
 //マウス座標
 int mouseX = 0;
@@ -14,7 +14,7 @@ int mouseY = 0;
 char buf[256] = { 0 };
 int keyState[256] = { 0 };
 
-int count = 0;
+int FrameCount = 0;
 
 int shot_img;
 int player_img;
@@ -23,59 +23,46 @@ int ResonanceAtTwilight_audio;
 //プレイヤーの座標
 int px = 320;
 int py = 320;
-const int moveSpeed = 4;
-float slow = 1;
-
-//キー入力状態を更新する関数
-void KeyUpdate(void)
-{
-	GetHitKeyStateAll(buf);
-	for (int i = 0; i < 256; i++)
-	{
-		if (buf[i] == 1) {
-			keyState[i]++;
-		}
-		else {
-			keyState[i] = 0;
-		}
-	}
-}
 
 void PlayerMove(void)
 {
+	const int MOVE_SPEED = 4;
+	float slow = 1;
+	const int PLAYER_SIZE_X = 10;
+	const int PLAYER_SIZE_Y = 20;
+
 	if (keyState[KEY_INPUT_LSHIFT] > 0) slow = 0.5;
 	else slow = 1;
 
-	if (keyState[KEY_INPUT_RIGHT] > 0 && px < Window_Width) //継続入力
+	if (keyState[KEY_INPUT_RIGHT] > 0 && px < WINDOW_WIDTH - PLAYER_SIZE_X) //継続入力
 	{
-		px += moveSpeed * slow;
+		px += MOVE_SPEED * slow;
 	}
-	if (keyState[KEY_INPUT_LEFT] > 0 && px > 0) //継続入力
+	if (keyState[KEY_INPUT_LEFT] > 0 && px > 0 + PLAYER_SIZE_X) //継続入力
 	{
-		px -= moveSpeed * slow;
+		px -= MOVE_SPEED * slow;
 	}
-	if (keyState[KEY_INPUT_DOWN] > 0 && py < Window_Height) //継続入力
+	if (keyState[KEY_INPUT_DOWN] > 0 && py < WINDOW_HEIGHT - PLAYER_SIZE_Y) //継続入力
 	{
-		py += moveSpeed * slow;
+		py += MOVE_SPEED * slow;
 	}
-	if (keyState[KEY_INPUT_UP] > 0 && py > 0) //継続入力
+	if (keyState[KEY_INPUT_UP] > 0 && py > 0 + PLAYER_SIZE_Y) //継続入力
 	{
-		py -= moveSpeed * slow;
+		py -= MOVE_SPEED * slow;
 	}
 }
 
 void Update(void) //毎フレーム処理
 {
 	DrawRotaGraph(300, 100, 5.0, PI/4, shot_img, TRUE); //画像の描画
-	DrawPixel(Window_Width / 2, Window_Height / 2, GetColor(255, 255, 255));	// 点を打つ
+	DrawPixel(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, GetColor(255, 255, 255));	// 点を打つ
 	DrawCircle(100, 100, 30, GetColor(255, 0, 0), 0);
 	DrawCircle(200, 100, 30, GetColor(0, 255, 0), 1);
-	DrawBox(Window_Width-250, 0, Window_Width, Window_Height, GetColor(0, 0, 255), 1);
-	DrawLine(200, 200, 300, 330, GetColor(255, 255, 255), 1);
+	DrawBox(WINDOW_WIDTH-250, 0, WINDOW_WIDTH, WINDOW_HEIGHT, GetColor(0, 0, 255), 1);
 	
-	count++;
-	DrawFormatString(Window_Width-100, 100, GetColor(255, 255, 255), "frame %d", count);
-	DrawFormatString(Window_Width-100, 120, GetColor(255, 255, 255), "sec %d", count / 60);
+	FrameCount++;
+	DrawFormatString(WINDOW_WIDTH-100, 100, GetColor(255, 255, 255), "frame %d", FrameCount);
+	DrawFormatString(WINDOW_WIDTH-100, 120, GetColor(255, 255, 255), "sec %d", FrameCount / 60);
 
 	GetMousePoint(&mouseX, &mouseY); //マウス座標更新
 	DrawFormatString(20, 50, GetColor(255, 255, 255), "MX:%3d MY:%3d", mouseX, mouseY); //左上に文字（マウスの座標）を描画
@@ -96,6 +83,17 @@ void Update(void) //毎フレーム処理
 	
 }
 
+//キー入力状態を更新する関数
+void KeyUpdate(void)
+{
+	GetHitKeyStateAll(buf);
+	for (int i = 0; i < 256; i++)
+	{
+		if (buf[i] == 1) keyState[i]++;
+		else keyState[i] = 0;
+	}
+}
+
 void ImgInit(void)
 {
 	shot_img = LoadGraph("shot.png");
@@ -111,7 +109,7 @@ void AudioInit(void)
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	ChangeWindowMode(TRUE);//非全画面にセット
-	SetGraphMode(Window_Width, Window_Height, 32);//画面サイズ指定
+	SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32);//画面サイズ指定
 	SetOutApplicationLogValidFlag(FALSE);//Log.txtを生成しないように設定
 	SetMainWindowText("鏡像の歌姫 - Reflection of Diva -");
 
