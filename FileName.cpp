84d1f0b1c907dc0838902c5743cca2154d5a2209
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-16 03:23:29 +0900

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -20,18 +20,32 @@ int player_img;
 int status_img;
 int ResonanceAtTwilight_audio;
 
-//プレイヤーの座標
-int px = 320;
-int py = 320;
+//プレイヤー
+const int InitialPosX = 350;
+const int InitialPosY = 550;
+const int Player_HitBoxSize = 4;
+int px = InitialPosX;
+int py = InitialPosY;
 
-//敵の座標
-int enX[3];
-int enY[3];
-int enHitBoxSize[3];
+//敵
+class Enemy{
+public:
+	int enX;
+	int enY;
+	int enHitBoxSize;
+	Enemy(int x, int y, int size)
+	{
+		enX = x;
+		enY = y;
+		enHitBoxSize = size;
+	};
+};
+
+Enemy enemy[3]{ {120,80,20},{220,20,20},{320,220,20} };
 
 int FrameCount = 0;
 
-const int Player_HitBoxSize = 4;
+int Life = 10;
 
 void Update(void) //毎フレーム処理
 {
@@ -40,11 +54,7 @@ void Update(void) //毎フレーム処理
 	DrawBox(0, 0, 100, WINDOW_HEIGHT, GetColor(0, 0, 0), 1);
 	DrawRotaGraph(700, 300, 0.8, 0, status_img, TRUE); //画像の描画
 
-	FrameCount++;
-	DrawFormatString(WINDOW_WIDTH - 100, 100, GetColor(255, 255, 255), "frame %d", FrameCount);
-	DrawFormatString(WINDOW_WIDTH - 100, 120, GetColor(255, 255, 255), "sec %d", FrameCount / 60);
-	GetMousePoint(&mouseX, &mouseY); //マウス座標更新
-	DrawFormatString(20, 50, GetColor(255, 255, 255), "MX:%3d MY:%3d", mouseX, mouseY); //左上に文字（マウスの座標）を描画
+	DrawFormatString(WINDOW_WIDTH - 100, 120, GetColor(255, 255, 255), "sec %d", FrameCount++ / 60);
 
 	if (KeyState[KEY_INPUT_X] == TRUE) //単発入力
 	{
@@ -53,16 +63,22 @@ void Update(void) //毎フレーム処理
 		PlaySoundMem(ResonanceAtTwilight_audio, DX_PLAYTYPE_BACK);
 	}
 
-	enX[0] = 200;
-	enY[0] = 200;
-	enHitBoxSize[0] = 30;
+	for (int i = 0; i < 3; i++) 
+	{
+		DrawCircle(enemy[i].enX, enemy[i].enY, enemy[i].enHitBoxSize, GetColor(255, 0, 0), 1);
 
-	DrawCircle(enX[0], enY[0], enHitBoxSize[0], GetColor(255, 0, 0), 1);
+		//敵との座標チェック
+		float dis = sqrt(pow((double)enemy[i].enX - px, 2) + pow((double)enemy[i].enY - py, 2));
+		if (dis <= enemy[i].enHitBoxSize + Player_HitBoxSize)
+		{
+			//被弾判定
+			px = InitialPosX;
+			py = InitialPosY;
+			Life -= 1;
+		}
+	}
 
-	//敵との座標チェック
-	float dis = sqrt(pow((double)enX[0] -px, 2) + pow((double)enY[0] - py, 2));
-	DrawFormatString(WINDOW_WIDTH - 100, 300, GetColor(255, 255, 255), "%f" , dis);
-	if (dis <= 30 + Player_HitBoxSize) DrawFormatString(WINDOW_WIDTH - 100, 350, GetColor(255, 255, 255), "Hit");
+	DrawFormatString(WINDOW_WIDTH - 100, 6, GetColor(255, 255, 255), "Life : %d", Life);
 
 	PlayerMove(KeyState, &px, &py);//プレイヤーの移動
 	DrawRotaGraph(px, py, 1.0, 0, player_img, TRUE); //画像の描画
